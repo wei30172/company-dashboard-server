58a68d8dfcf5f43a418fa4dca7bef37d313d09ab
wei30172 <60259324+wei30172@users.noreply.github.com>
2022-08-08 11:21:57 -0700

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@company-dashboard.o7s1q4w.mongodb.net/`;
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 8080;
+const const SERVER_PORT = process.env.PORT || 9090;
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || "superIssuer";
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "superencryptedSecret";
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "superencryptedSecret";
